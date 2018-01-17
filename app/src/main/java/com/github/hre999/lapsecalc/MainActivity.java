816fdd4c71191a3f51f2dc364d823d29a47e49ef
hre999 <hre999>
2018-01-17 14:00:53 +0100

    changed Page1 UI for results added ActionListener

--- app/src/main/java/com/github/hre999/lapsecalc/MainActivity.java
+++ app/src/main/java/com/github/hre999/lapsecalc/MainActivity.java
@@ -5,58 +5,58 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Page Adapter for plugging the Nested XML into the ViewPager
-    /*class NestedXMLPagerAdapter extends PagerAdapter {
+    /////////////
+    // Constants
+    static final int pages = 3;
 
-        public Object instantiateItem(View collection, int position) {
-
-            int resId = 0;
-            switch (position) {
-                case 0:
-                    resId = R.id.group1;
-                    break;
-                case 1:
-                    resId = R.id.group2;
-                    break;
-                case 2:
-                    resId = R.id.group3;
-                    break;
-            }
-            return findViewById(resId);
-        }
-
-        @Override
-        public int getCount() {
-            return 3;
-        }
-
-        @Override
-        public boolean isViewFromObject(View arg0, Object arg1) {
-            return arg0 == ((View) arg1);
-        }
-    }*/
+    //////////
+    // Classes
 
     // PagerAdapter for plugging seperate XML into the ViewPager
     private class XMLPagerAdapter extends PagerAdapter {
 
+        // Listeners...apparently we put these here now?
+        TextView.OnEditorActionListener p1ActionListener = new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Toast.makeText(MainActivity.this, v.getText(), Toast.LENGTH_SHORT).show();
+                return false;   // return false so we still get the focus shift
+            }
+        };
+
         public int getCount() {
-            return 3;
+            return pages;
         }
 
         public Object instantiateItem(View collection, int position) {
 
+            // prep
             LayoutInflater inflater = (LayoutInflater) collection.getContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
             int resId = 0;
+            List<Integer> inputIds = new ArrayList();
+
+            // page details
             switch (position) {
                 case 0:
                     resId = R.layout.page1;
+                    inputIds.add(R.id.p1Capture);
+                    inputIds.add(R.id.p1Clip);
+                    inputIds.add(R.id.p1Fps);
                     break;
                 case 1:
                     resId = R.layout.page2;
@@ -66,9 +66,15 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
 
+            // Inflate and add layout
             View view = inflater.inflate(resId, null);
-
             ((ViewPager) collection).addView(view, 0);
+
+            // grab our inputs and attach the ActionListener
+            for(int id:inputIds){
+                EditText input = findViewById(id);
+                input.setOnEditorActionListener(p1ActionListener);
+            }
 
             return view;
         }
@@ -86,25 +92,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    ///////////
+    // Methods
+
+    // OnCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Set the ViewPager adapter
-        /*NestedXMLPagerAdapter adapter = new NestedXMLPagerAdapter();
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
-
-        pager.setOffscreenPageLimit(3);*/
-
-        // Set the ViewPager adapter
         XMLPagerAdapter adapter = new XMLPagerAdapter();
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(adapter);
-
+        pager.setOffscreenPageLimit(3);
 
     }
-
 
 }
